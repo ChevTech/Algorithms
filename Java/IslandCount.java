f0ad2c62ae8b1976f2d63086c9493b67ac701b8a
ChevTech <anton.stoytchev@gmail.com>
2018-03-06 23:35:47 -0500

    IslandCount by UnionFind optimized

--- Java/IslandCount.java
+++ Java/IslandCount.java
@@ -1,74 +1,90 @@
 public class IslandCount {
-    public int numIslands(char[][] grid) {
-        int n = grid.length;
-        if(n == 0) return 0;
+    class Solution {
+        public int numIslands(char[][] grid) {
+            int n = grid.length;
+            if(n == 0) return 0;
 
-        int m = grid[0].length;
-        if(m == 0) return 0;
+            int m = grid[0].length;
+            if(m == 0) return 0;
 
-        UnionFind uf = new UnionFind(n * m);
-        int ones = 0;
-        for(int row=0; row < n; row++){
-            for(int col=0; col < m; col++){
-                if(grid[row][col] == '1'){
-                    ones += 1;
-                    int p = row * m + col;
+            UnionFind uf = new UnionFind(n * m);
+            int ones = 0;
+            for(int row=0; row < n; row++){
+                for(int col=0; col < m; col++){
+                    if(grid[row][col] == '1'){
+                        ones += 1;
+                        int p = row * m + col;
 
-                    if(col + 1 < m && grid[row][col+1] == '1'){
-                        uf.union(p, row * m + (col + 1));
-                    }
+                        if(col + 1 < m && grid[row][col+1] == '1'){
+                            uf.union(p, row * m + (col + 1));
+                        }
 
-                    if(row + 1 < n && grid[row+1][col] == '1'){
-                        uf.union(p, (row + 1) * m + col);
+                        if(row + 1 < n && grid[row+1][col] == '1'){
+                            uf.union(p, (row + 1) * m + col);
+                        }
                     }
                 }
             }
+
+            return uf.getIslands(ones);
         }
 
-        return uf.getIslands(ones);
-    }
+        class UnionFind {
+            int[] id;
+            int[] size;
 
-    class UnionFind {
-        int[] id;
-
-        public UnionFind(int n){
-            id = new int[n];
-            for(int i=0; i < n; i++){
-                id[i] = i;
-            }
-        }
-
-        public int root(int i){
-            while(i != id[i]){
-                i = id[i];
+            public UnionFind(int n){
+                id = new int[n];
+                size = new int[n];
+                for(int i=0; i < n; i++){
+                    id[i] = i;
+                    size[i] = 1;
+                }
             }
 
-            return i;
-        }
+            public int root(int i){
+                /* Flatten tree */
+                while(i != id[id[i]]){
+                    id[i] = id[id[i]];
+                    i = id[i];
+                }
 
-        public boolean find(int p, int q){
-            return root(p) == root(q);
-        }
+                return i;
+            }
 
-        public void union(int p, int q){
-            int i = root(p);
-            int j = root(q);
-            id[i] = j;
-        }
+            public boolean find(int p, int q){
+                return root(p) == root(q);
+            }
 
-        public int getIslands(int totalOnes){
-            Set<Integer> seen = new HashSet<>();
-            int connected = 0;
-            for(int i=0; i < id.length; i++){
-                int rt = root(i);
-                if(seen.contains(rt)){
-                    connected++;
+            public void union(int p, int q){
+                int i = root(p);
+                int j = root(q);
+
+                /* Add to smallest tree */
+                if(size[i] < size[j]){
+                    id[i] = j;
+                    size[i] += size[j];
                 } else {
-                    seen.add(rt);
+                    id[j] = i;
+                    size[j] += size[i];
                 }
+
             }
 
-            return totalOnes - connected;
+            public int getIslands(int totalOnes){
+                Set<Integer> seen = new HashSet<>();
+                int connected = 0;
+                for(int i=0; i < id.length; i++){
+                    int rt = root(i);
+                    if(seen.contains(rt)){
+                        connected++;
+                    } else {
+                        seen.add(rt);
+                    }
+                }
+
+                return totalOnes - connected;
+            }
         }
     }
 }
